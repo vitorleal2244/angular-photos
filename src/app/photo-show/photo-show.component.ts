61ea89b02f0b7c57d2746945c19ade3f9bbcdc16
vlopesle <vitor-andre.lopes-leal@capgemini.com>
2023-04-05 08:54:56 +0100

    [Angular][Angular-Photos] - Finished app

--- src/app/photo-show/photo-show.component.ts
+++ src/app/photo-show/photo-show.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { PhotosService } from '../photos.service';
 
 @Component({
   selector: 'app-photo-show',
   templateUrl: './photo-show.component.html',
-  styleUrls: ['./photo-show.component.css']
+  styleUrls: ['./photo-show.component.css'],
 })
 export class PhotoShowComponent {
+  public photoUrl: string;
 
+  constructor(private photosService: PhotosService) {
+    this.photoUrl = '';
+
+    this.fetchPhoto();
+  }
+
+  private fetchPhoto() {
+    this.photosService.getPhoto().subscribe((response) => {
+      this.photoUrl = response.urls.regular;
+    });
+  }
+
+  public onClick(): void {
+    this.fetchPhoto();
+  }
 }
--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
+
 const unsplash_link = 'https://api.unsplash.com';
 
 @Injectable({
@@ -10,7 +16,7 @@ export class PhotosService {
   constructor(private http: HttpClient) {}
 
   getPhoto() {
-    return this.http.get(unsplash_link + '/photos/random', {
+    return this.http.get<UnsplashResponse>(unsplash_link + '/photos/random', {
       headers: {
         Authorization: 'Client-ID 43ONN0bymhiu1had_m0w-Bkkq1yJP-7c_hdJrudIBu4',
       },
